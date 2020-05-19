df3f4e612d70c22cd29bb3595a19e70a9f0f07a8
helenecharlotte <helenerytgaard@gmail.com>
2020-05-19 09:12:41 +0200

    scripts for running simulations and showing output

--- simulation/run.R
+++ simulation/run.R
@@ -62,28 +62,35 @@ source("./simulation/compute-true.R")
 ## set parameters
 #-------------------------------------------------------------------------------------------#
 
-K <- 5#5#100#100#100#100#80#100
-run.ltmle <- FALSE##TRUE#FALSE
-run.ctmle <- FALSE#FALSE#FALSE
-run.ctmle2 <- TRUE#FALSE#FALSE
-compute.true.eic <- FALSE
-compute.true.psi <- FALSE
+K <- 100
+run.ltmle <- FALSE
+run.ctmle2 <- TRUE
 misspecify.Q <- FALSE
 only.A0 <- FALSE
-M <- 300
+M <- 500
+n <- 1000
+
+#-------------------------------------------------------------------------------------------#
+## don't change here
+#-------------------------------------------------------------------------------------------#
+
+run.ctmle <- TRUE
 
 #-------------------------------------------------------------------------------------------#
 ## true values (outputs to file)
 #-------------------------------------------------------------------------------------------#
 
+compute.true.eic <- FALSE
+compute.true.psi <- FALSE
+
 compute.true(compute.true.psi=compute.true.psi, compute.true.eic=compute.true.eic, only.A0=only.A0)
 
 #-------------------------------------------------------------------------------------------#
-## repeat simulations (parallelize
+## repeat simulations (parallelize)
 #-------------------------------------------------------------------------------------------#
 
 if (system("echo $USER",intern=TRUE)%in%c("jhl781")){ 
-    no_cores <- 25
+    no_cores <- 30
 } else {
     no_cores <- detectCores() - 1
 }
@@ -92,7 +99,7 @@ registerDoParallel(no_cores)
 
 out <- foreach(m=1:M, .errorhandling="pass"#, #.combine=list, .multicombine = TRUE
                ) %dopar% {
-                   repeat.fun(m, K=K,
+                   repeat.fun(m, K=K, n=n,
                               only.A0=only.A0, run.ltmle=run.ltmle, run.ctmle=run.ctmle,
                               run.ctmle2=run.ctmle2,
                               misspecify.Q=misspecify.Q)
@@ -105,7 +112,8 @@ saveRDS(out,
                     "outlist-est",
                     ifelse(run.ltmle, "-ltmle", ""),
                     ifelse(run.ctmle, "-ctmle", ""),
-                    ifelse(run.ctmle2, "-ctmle2", ""), 
+                    ifelse(run.ctmle2, "-ctmle2", ""),
+                    ifelse(n==1000, "", paste0("-n", n)), 
                     "-2020", "-K", K, ifelse(only.A0, "-A0", ""), ifelse(misspecify.Q, "-Q", ""), 
                     "-M", M, ".rds"))
 
--- simulation/show.R
+++ simulation/show.R
@@ -1,16 +1,80 @@
+### show.R --- 
+#----------------------------------------------------------------------
+## Author: Helene Charlotte Rytgaard
+## Created: May, 2020 
+#----------------------------------------------------------------------
+## 
+### Commentary:
+## Look at output from TMLE estimation.
+##  
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+#-------------------------------------------------------------------------------------------#
+## packages and generic functions
+#-------------------------------------------------------------------------------------------#
+
+library(data.table)
+library(zoo)
+library(stringr)
+library(ltmle)
+library(parallel)
+library(foreach)
+library(doParallel)
+
+numextract <- function(string){ 
+    as.numeric(str_extract(string, "\\-*\\d+\\.*\\d*"))
+}
+
+logit <- function(p) log(p/(1-p)) #qlogis :) 
+mse <- function(x, x0=NULL) {
+    if (length(x0)==0) {
+        return(sqrt(mean((x-mean(x))^2)))
+    } else {
+        return(sqrt(mean((x-x0)^2)))
+    }
+}
+
+#-------------------------------------------------------------------------------------------#
+## set working directory
+#-------------------------------------------------------------------------------------------#
+
+if (system("echo $USER",intern=TRUE)%in%c("jhl781")){
+    setwd("/home/ifsv/jhl781/research/phd/berkeley/continuousTMLE/")
+} else {
+    setwd("~/research/phd/berkeley/continuousTMLE/")
+}
+
+#-------------------------------------------------------------------------------------------#
+## source relevant scripts
+#-------------------------------------------------------------------------------------------#
+
+source("./R/sim-data.R")
+source("./R/est-fun.R")
+source("./R/fit-density-by-hazard.R")
+source("./simulation/coverage-fun.R")
+source("./simulation/repeat-fun.R")
+source("./simulation/compute-true.R")
+
 #-------------------------------------------------------------------------------------------#
 ## parameters of interest
 #-------------------------------------------------------------------------------------------#
 
-K <- 10#20#100#100#100#100#80#100
-run.ltmle <- FALSE##TRUE#FALSE
-run.ctmle <- FALSE#FALSE#FALSE
-run.ctmle2 <- TRUE#FALSE#FALSE
-compute.true.eic <- FALSE
+K <- 100
+run.ltmle <- FALSE
+run.ctmle2 <- TRUE
 misspecify.Q <- FALSE
 misspecify.Q <- FALSE
 only.A0 <- FALSE
-M <- 300
+M <- 500
+n <- 1000
+
+#-------------------------------------------------------------------------------------------#
+## don't change here
+#-------------------------------------------------------------------------------------------#
+
+run.ctmle <- TRUE
 
 #-------------------------------------------------------------------------------------------#
 ## get output from simulations
@@ -20,7 +84,8 @@ length(out <- readRDS(file=paste0("./simulation/output/",
                                   "outlist-est",
                                   ifelse(run.ltmle, "-ltmle", ""),
                                   ifelse(run.ctmle, "-ctmle", ""),
-                                  ifelse(run.ctmle2, "-ctmle2", ""), 
+                                  ifelse(run.ctmle2, "-ctmle2", ""),
+                                  ifelse(n==1000, "", paste0("-n", n)), 
                                   "-2020", "-K", K, ifelse(only.A0, "-A0", ""),
                                   ifelse(misspecify.Q, "-Q", ""), 
                                   "-M", M, ".rds")))
@@ -31,23 +96,15 @@ length(out <- readRDS(file=paste0("./simulation/output/",
 
 (psi0.A0 <- readRDS(file=paste0("./simulation/output/",
                                 "outlist-est-true-0-2020",
-                                "-K", K, #ifelse(misspecify.Q, "-Q", ""),
+                                "-K", K, 
+                                ifelse(only.A0, "-A0", ""),
                                 "-M", "1", ".rds")))
 
 (psi0.A1 <- readRDS(file=paste0("./simulation/output/",
                                 "outlist-est-true-1-2020",
-                                "-K", K, #ifelse(misspecify.Q, "-Q", ""),
+                                "-K", K, 
+                                ifelse(only.A0, "-A0", ""),
                                 "-M", "1", ".rds")))
-
-(true.eic.A0 <- readRDS(file=paste0("./simulation/output/",
-                                    "outlist-est-true-sd-0-2020",
-                                    "-K", K, #ifelse(misspecify.Q, "-Q", ""),
-                                    "-M", "1", ".rds")))
-
-(true.eic.A1 <- readRDS(file=paste0("./simulation/output/",
-                                    "outlist-est-true-sd-1-2020",
-                                    "-K", K, #ifelse(misspecify.Q, "-Q", ""),
-                                    "-M", "1", ".rds")))
 
 #-------------------------------------------------------------------------------------------#
 ## extract results of interest
@@ -84,20 +141,39 @@ psiA1.init <- unlist(lapply(out, function(xout, A=1, which=1) {
 sdA0 <- unlist(lapply(out, function(xout, A=0, which=3) {
     if (xout[1]!="ERROR") {
         yout <- xout[[2-A]]
-        if (run.ltmle) yout[2] else yout[[length(yout)]][which]
+        if (run.ltmle) yout[2] else yout[[1]][which]
     }
 }))
 
 sdA1 <- unlist(lapply(out, function(xout, A=1, which=3) {
     if (xout[1]!="ERROR") {
         yout <- xout[[2-A]]
-        if (run.ltmle) yout[2] else yout[[length(yout)]][which]
+        if (run.ltmle) yout[2] else yout[[1]][which]
     }
 }))
 
 (count.na <- mean(unlist(lapply(out, function(xout, A=1, which=3) {
     1*(xout[1]=="ERROR")
 }))))
+
+if (FALSE) {
+
+    hist(sdA1)
+    hist(sdA0)
+
+    par(mfrow=c(2,2))
+    hist(psiA0.init)
+    abline(v=psi0.A0, col="red")
+    hist(psiA1.init)
+    abline(v=psi0.A1, col="red")        
+    hist(psiA0)
+    abline(v=psi0.A0, col="red")
+    hist(psiA1)
+    abline(v=psi0.A1, col="red")        
+    par(mfrow=c(1, 1))
+    
+}
+
 
 #-------------------------------------------------------------------------------------------#
 ## extract results of interest
@@ -118,6 +194,7 @@ message("-----------------------------------")
 message("look at coverage of tmle estimator:")
 message(paste0("coverage (A=0): ", round(cov.fun(psiA0, sdA0, psi0.A0), 4)))
 message(paste0("coverage (A=1): ", round(cov.fun(psiA1, sdA1, psi0.A1), 4)))
+message(paste0("coverage (diff): ", round(cov.fun(psiA1-psiA0, sqrt(sdA1^2+sdA0^2), psi0.A1-psi0.A0), 4)))
 
 message("----------------------------")
 message("look at se estimates (A=0):")
@@ -125,17 +202,16 @@ message(paste0("mean sigma : ", round(mean(sdA0), 4)))
 message(paste0("mse        : ", round(mse(psiA0), 4)))
 
 message("----------------------------")
-message("look at se estimates (A=0):")
+message("look at se estimates (A=1):")
 message(paste0("mean sigma : ", round(mean(sdA1), 4)))
 message(paste0("mse        : ", round(mse(psiA1), 4)))
 
 message("-------------------------------")
 message("look at efficiency (A=0):")
 message(paste0("MSE / sd       : ", round(mse(psiA0)/mean(sdA0), 4)))
-message(paste0("MSE / true eic : ", round(mse(psiA0)/true.eic.A0, 4)))
 
 message("-------------------------------")
 message("look at efficiency (A=1):")
 message(paste0("MSE / sd       : ", round(mse(psiA1)/mean(sdA1), 4)))
-message(paste0("MSE / true eic : ", round(mse(psiA1)/true.eic.A1, 4)))
+
 
